8ee812e0109510b4f5ef8e7bec52203d0fd0f68d
Ali Ghavampour <alighavam79@gmail.com>
2023-08-14 19:19:04 -0400

    Added comments. Fixed minor issues.
    Added comments to the code.
    Fixed an issue with the naming extension for creating different VDM files.
    Change the location of some lines to achieve more clarity and intuition.

--- spmj_makefieldmap.m
+++ spmj_makefieldmap.m
@@ -24,67 +24,144 @@ function spmj_makefieldmap(dataDir, subj_name, run, varargin)
 % Added support for 3D files while keeping backward compatibility for 4D
 % files
 
-prefix='a'; 
-image=1; 
-subfolderRawdata='';
-subfolderFieldmap='';
-rawdataDir=''; 
+% Prefix of the EPI data:
+prefix = '';
 
-use3D=false;
+% Which image (volume) to unwrap. This is mainly for the quality control 
+% of field map correction. The corrected image will be saved with
+% the prefix 'u'. 
+image = 1;
 
-vararginoptions(varargin,{'prefix', 'image', 'subfolderRawdata', 'subfolderFieldmap', 'use3D','rawdataDir'}); 
-spm_dir= fileparts(which('spm'));
-spmVer=spm('Ver');
+% Directory of the raw EPI data:
+rawdataDir = '';
 
+% Subfolder of the Raw EPI data:
+subfolderRawdata = '';
+
+% Subfolder of the field map data:
+subfolderFieldmap = '';
+
+% Option to use 3D images:
+use3D = false;
+
+% Handling the input arguments:
+vararginoptions(varargin,{'prefix', 'image', 'subfolderRawdata', 'subfolderFieldmap', 'use3D','rawdataDir'});
+
+% Directory of the spm toolbox:
+spm_dir = fileparts(which('spm'));
+
+% Version of spm currently being used:
+spmVer = spm('Ver');
 
 % displaying whether using 3D files or not
 % disp(['Using 3D: ' num2str(use3D)])
 
-%_______DEFAULTS_________________________________
-J.defaults.defaultsval.et = [10 12.43];                                                                 
-J.defaults.defaultsval.maskbrain = 1;                                                                   
-J.defaults.defaultsval.blipdir = -1;                                                                    
-J.defaults.defaultsval.tert = 53.76;                                                                    
-J.defaults.defaultsval.epifm = 0;                                                                       
-J.defaults.defaultsval.ajm = 0;                                                                         
-J.defaults.defaultsval.uflags.method = 'Mark3D';                                                        
-J.defaults.defaultsval.uflags.fwhm = 10;                                                                
-J.defaults.defaultsval.uflags.pad = 0;                                                                  
-J.defaults.defaultsval.uflags.ws = 1;   
+
+% Parameters for the SPM field map toolbox:
+
+%_______DEFAULTS Values_________________________________
+% Echo times for magnitude images. Can be found in the meta data of the images. Default parameter for CFMM 3T scanner:
+J.defaults.defaultsval.et = [4.92 7.38];
+
+% If masking brain is on, the magnitude image is used to mask the brain:
+J.defaults.defaultsval.maskbrain = 1;
+
+% Phase-enocding direction. Should be available in the imaging sequence specifications. 
+% Spm defines Anterior to Posterior as -1 and Posterior to Anterior as +1:
+J.defaults.defaultsval.blipdir = -1;
+
+% Total EPI readout time = echo spacing (in ms) * base resolution (also knows as number of echos).  
+% If you use GRAPPA acceleration, you need to divide the total number of echos by two:
+J.defaults.defaultsval.tert = 0.7 * 90 * 1/2;
+
+% EPI fieldmap or non-EPI. In CFMM 3T gradient echo imaging is used for
+% field map acquisition which is a non-EPI sequence:
+J.defaults.defaultsval.epifm = 0;
+
+% Whether to apply Jacobian Modulation:
+J.defaults.defaultsval.ajm = 0;
+
+% DEFAULTS Values - uflags: for phase unwrapping and field map processing
+% The unwrapping method:
+J.defaults.defaultsval.uflags.method = 'Mark3D';
+
+% FWHM of the Gaussian filter used to implement weighted smoothing of
+% unwrapped images:
+J.defaults.defaultsval.uflags.fwhm = 10;
+
+% Size of padding kernel if required:
+J.defaults.defaultsval.uflags.pad = 0;
+
+% Normal or weighted smoothing:
+J.defaults.defaultsval.uflags.ws = 1;
+
+% DEFAULTS Values - mflags: for segmentation and creation of the brain mask
+% Template file for segmentation to create brain mask:
 switch (spmVer)
     case 'SPM12'
-        J.defaults.defaultsval.mflags.template = {fullfile(spm_dir,'canonical','avg152T1.nii')}; 
+        J.defaults.defaultsval.mflags.template = {fullfile(spm_dir,'canonical','avg152T1.nii')};
     case 'SPM8'
-        J.defaults.defaultsval.mflags.template = {fullfile(spm_dir,'templates','T1.nii')}; 
-end; 
-J.defaults.defaultsval.mflags.fwhm = 5;                                                                 
-J.defaults.defaultsval.mflags.nerode = 2;                                                               
-J.defaults.defaultsval.mflags.ndilate = 4;                                                              
-J.defaults.defaultsval.mflags.thresh = 0.5;                                                             
-J.defaults.defaultsval.mflags.reg = 0.02;                                                               
-J.matchvdm = 1;                                                                                                                                                                              
-J.writeunwarped = 1;                                                                                    
-J.anat = [];                  
-J.matchanat = 0; 
+        J.defaults.defaultsval.mflags.template = {fullfile(spm_dir,'templates','T1.nii')};
+end
+
+% FWHM of Gaussian filter for smoothing brain mask:
+J.defaults.defaultsval.mflags.fwhm = 5;
+
+% Number of erosions used to create brain mask:
+J.defaults.defaultsval.mflags.nerode = 2;
+
+% Number of dilations used to create brain mask:
+J.defaults.defaultsval.mflags.ndilate = 4;
+
+% Threshold used to create brain mask from segmented data:
+J.defaults.defaultsval.mflags.thresh = 0.5;
+
+% value used in the segmentation. A larger value helps the segmentation to 
+% converge:
+J.defaults.defaultsval.mflags.reg = 0.02;
+
+%_______EPI Sessions, for multiple runs with same fieldmap_________________
+% Match VDM file to EPI image. This will coregister the field map data to
+% the selected EPI for each run/session:
+J.matchvdm = 1;
+
+% This will be the name extension followed by an incremented integer for
+% run/session specific VDM files:
+J.sessname = 'run_';
+
+% Write out distortion corrected EPI image. The image is saved with the 
+% prefix 'u'. This is mainly for the quality control of field map correction: 
+J.writeunwarped = 1;
+
+% Select an anatomical image for comparison with the distortion corrected EPI:
+J.anat = [];
+
+% Match the anatomical image to the distortion corrected EPI:
+J.matchanat = 0;
 
 
 if (isempty(rawdataDir))
-    rawdataDir=fullfile(dataDir, 'imaging_data_raw', subj_name, subfolderRawdata); 
-end; 
+    rawdataDir=fullfile(dataDir, 'imaging_data_raw', subj_name, subfolderRawdata);
+end
 
-%_______for multiple run with same fieldmap_________________________________
-for i=1:numel(run) 
+% Adding the EPI sessions/runs images:
+for i=1:numel(run)
     if use3D
         J.session(i).epi ={fullfile(rawdataDir, [prefix subj_name,'_run',run{i},'_',num2str(image),'.nii'])};
     else
-        J.session(i).epi ={fullfile(rawdataDir, [prefix,subj_name,'_run_',run{i},'.nii,',num2str(image)])};    
-    end;
+        J.session(i).epi ={fullfile(rawdataDir, [prefix,subj_name,'_run_',run{i},'.nii,',num2str(image)])};
+    end
 end
-J.sessname = {'run_RENAMErunNumber'};%{['run_', run{i}]};
-%_______change code here if we have 1 fieldmap for each run_________________________________
-J.phase ={fullfile(dataDir, 'fieldmaps', subj_name, subfolderFieldmap, [subj_name,'_phase.nii,1'])};          %,'_',num2str(run(1))
-J.magnitude =  {fullfile(dataDir, 'fieldmaps', subj_name, subfolderFieldmap, [subj_name,'_magnitude.nii,1'])};  %,'_',num2str(run(1))
 
+% Path to the phase image - Change the next two lines if you have a field
+% map for each run:
+J.phase ={fullfile(dataDir, 'fieldmaps', subj_name, subfolderFieldmap, [subj_name,'_phase.nii,1'])}; %,'_',num2str(run(1))
+
+% Path to the magnitude image:
+J.magnitude =  {fullfile(dataDir, 'fieldmaps', subj_name, subfolderFieldmap, [subj_name,'_magnitude.nii,1'])}; %,'_',num2str(run(1))
+
+% Creating the Batch for the SPM:
 matlabbatch{1}.spm.tools.fieldmap.presubphasemag.subj= J;
 
+% Passing the created job to SPM:
 spm_jobman('run',matlabbatch);

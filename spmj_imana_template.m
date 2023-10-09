f9cd4be76197b6f6ff3f7d1b9a6fe630ba5076c9
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2023-10-09 14:09:35 -0400

    Update spmj_imana_template.m

--- spmj_imana_template.m
+++ spmj_imana_template.m
@@ -124,8 +124,235 @@ switch(what)
                           'subfolderFieldmap',subfolderFieldmap);
             end
         end
+<<<<<<< Updated upstream
     case 'GLM:make_glm_1'
         
+=======
+    case 'GLM:make_glm_1'   
+        % make the design matrix for the glm
+        % models each condition as a separate regressors
+        % For conditions with multiple repetitions, one regressor
+        % represents all the instances
+        % nishimoto_imana('GLM:design1', 'sn', [6])
+        
+        sn = subj_id;
+        hrf_cutoff = Inf;
+        prefix = 'r'; % prefix of the preprocessed epi we want to use
+        glm = 1;
+        vararginoptions(varargin, {'sn', 'hrf_cutoff', 'ses'});
+        
+
+        % get the info file that specifies the the tasks and order?
+        Dd = dload(fullfile(base_dir, 'task_description.tsv'));
+        
+        for s = sn
+                func_subj_dir = fullfile(base_dir, func_dir,subj_str{s});
+ 
+                % loop through runs within the current sessions
+                itaskUni = 0;
+                for ses = [1]
+                 % create a directory to save the design
+                  subj_est_dir = fullfile(base_dir, glm_first_dir,subj_str{s}, sprintf('ses-%02d',ses));
+                  dircheck(subj_est_dir)
+                  
+                  T = []; % task/condition + session + run info
+                  J = []; % structure with SPM fields to make the design
+                 
+                  J.dir            = {subj_est_dir};
+                  J.timing.units   = 'secs';
+                  J.timing.RT      = 1.3;
+                  J.timing.fmri_t  = 16;
+                  J.timing.fmri_t0 = 8;
+                  
+                    % get the list of runs for the current session
+                    runs = run_list{ses};
+                    for run = 1:2 %length(runs)
+                       %V = spm_vol(fullfile(base_dir,func_dir, subj_str{s},sprintf('ses-%02d', ses),sprintf('r%s_run-%02d.nii', subj_str{s}, run)));
+                       %numTRs = length(V);
+             
+                       % get the path to the tsv file
+                       tsv_path = fullfile(base_dir, func_dir,subj_str{s});
+                       % get the tsvfile for the current run
+                       D = dload(fullfile(tsv_path,sprintf('ses-%02d',ses), sprintf('run%d.tsv', run)));
+                       
+                       % Get the onset and duration of the last sentence
+                       lastSentenceOnset = D.onset(end);
+                       lastSentenceDuration = D.duration(end);
+                       
+                       % Convert the end time of the last sentence to TRs
+                       endTimeInTRs = ceil((lastSentenceOnset + lastSentenceDuration) / J.timing.RT);
+
+
+                       % Define scans up to the last sentence's end time
+                       N = cell(endTimeInTRs - numDummys, 1);
+                       
+                       for i = 1:(endTimeInTRs - numDummys)
+                           N{i} = fullfile(func_subj_dir, sprintf('ses-%02d', ses), sprintf('%s%s_run-%02d.nii, %d', prefix, subj_str{s}, run, i+numDummys)); % to exclude dummy volumes
+                       end % i (image numbers)
+                       J.sess(run).scans = N; % scans in the current runs
+                        
+                       % loop over trials within the current run and build up
+                       % the design matrix
+                       for ic = 1:length(Dd.task_name)
+                           itaskUni = itaskUni+1;
+                           % get the indices corresponding to the current
+                           % condition.
+                           % this line is necessary as there are some
+                           % conditions with more than 1 repetition
+                           idx = strcmp(D.trial_type, Dd.task_name{ic});
+                           fprintf('* %d instances found for condition %s in run %02d\n', sum(idx), Dd.task_name{ic}, run)
+                            
+                           %
+                           % filling in "reginfo"
+                           TT.sn        = s;
+                           TT.sess      = ses;
+                           TT.run       = run;
+                           TT.task_name = Dd.task_name(ic);
+                           TT.task      = ic;
+                           TT.taskUni   = itaskUni;
+                           TT.n_rep     = sum(idx);
+                            
+                           % filling in fields of J (SPM Job)
+                           J.sess(run).cond(ic).name = Dd.task_name{ic};
+                           J.sess(run).cond(ic).tmod = 0;
+                           J.sess(run).cond(ic).orth = 0;
+                           J.sess(run).cond(ic).pmod = struct('name', {}, 'param', {}, 'poly', {});
+                            
+                           % get onset and duration (should be in seconds)
+                           onset    = D.onset(idx) - (J.timing.RT*numDummys);
+                           fprintf("The onset is %f\n", onset)
+                           if onset < 0
+                               warning("negative onset found")
+                           end
+                           duration = D.duration(idx);
+                           fprintf("The duration is %f\n", duration);
+                            
+                           J.sess(run).cond(ic).onset    = onset;
+                           J.sess(run).cond(ic).duration = duration;
+                            
+                           % add the condition info to the reginfo structure
+                           T = addstruct(T, TT);
+                            
+                            
+                        end % ic (conditions)
+                        
+                       J.sess(run).multi     = {''};
+                       J.sess(run).regress   = struct('name', {}, 'val', {});
+                       J.sess(run).multi_reg = {''};
+                       J.sess(run).hpf       = hrf_cutoff; % set to 'inf' if using J.cvi = 'FAST'. SPM HPF not applied
+                   end % run (runs of current session)
+                
+                
+               J.fact             = struct('name', {}, 'levels', {});
+               J.bases.hrf.derivs = [0 0];
+               J.bases.hrf.params = [4.5 11];                                  % set to [] if running wls
+               J.volt             = 1;
+               J.global           = 'None';
+               J.mask             = {fullfile(func_subj_dir,'ses-01','rmask_noskull.nii')};
+               J.mthresh          = 0.05;
+               J.cvi_mask         = {fullfile(func_subj_dir, 'ses-01', 'rmask_gray.nii')};
+               J.cvi              =  'fast';
+                
+               spm_rwls_run_fmri_spec(J);
+                
+                
+               dsave(fullfile(J.dir{1},sprintf('%s_reginfo.tsv', subj_str{s})), T);
+               fprintf('- estimates for glm_%d session %d has been saved for %s \n', glm, ses, subj_str{s});
+             end % ses (session)
+            
+            
+        end % sn (subject)  
+    
+    case 'GLM:estimate'      % estimate beta values
+        % Example usage: nishimoto_imana('GLM:estimate', 'glm', 1, 'ses', 1, 'sn', 6)
+        
+        sn       = subj_id; % subject list
+        sessions   = [1];       % session number
+        
+        vararginoptions(varargin, {'sn', 'sessions'})
+        
+        for s = sn
+         
+            for ses = sessions
+                fprintf('- Doing glm estimation for session %02d %s\n', ses, subj_str{s});
+                subj_est_dir = fullfile(base_dir, glm_first_dir,subj_str{s}, sprintf('ses-%02d', ses));         
+            
+                load(fullfile(subj_est_dir,'SPM.mat'));
+                SPM.swd = subj_est_dir;
+            
+                spm_rwls_spm(SPM);
+            end
+        end % s (sn),  
+         
+        
+    case 'GLM:T_contrast'    % make T contrasts for each condition
+        %%% Calculating contrast images.
+        % Example usage: nishimoto_imana('GLM:T_contrast', 'sn', 2, 'glm', 1, 'ses', 1, 'baseline', 'rest')
+        
+        sn             = subj_id;    % subjects list
+        ses            = 1;              % task number
+        glm            = 1;              % glm number
+        baseline       = 'rest';         % contrast will be calculated against base (available options: 'rest')
+        
+        vararginoptions(varargin, {'sn', 'glm', 'ses', 'baseline'})
+        
+        for s = sn
+            
+            % get the subject id folder name
+            fprintf('Contrasts for session %02d %s\n', ses, subj_str{s})
+            glm_dir = fullfile(base_dir, glm_first_dir, subj_str{s}, ses_str{ses}); 
+            
+            cd(glm_dir);
+            
+            % load the SPM.mat file
+            load(fullfile(glm_dir, 'SPM.mat'))
+            
+            SPM  = rmfield(SPM,'xCon');
+            T    = dload(fullfile(glm_dir, sprintf('%s_reginfo.tsv', subj_str{s})));
+            
+            % t contrast for each condition type
+            utask = unique(T.task)';
+            idx = 1;
+            for ic = utask
+                switch baseline
+                    case 'myBase' % contrast vs future baseline :)))
+                        % put your new contrasts here!
+                    case 'rest' % contrast against rest
+                        con                          = zeros(1,size(SPM.xX.X,2));
+                        con(:,logical((T.task == ic)& (T.n_rep>0))) = 1;
+%                         n_rep = length(T.run(T.task == ic));
+%                         n_rep_t = T.n_rep(T.task == ic);
+%                         name = unique(T.task_name(T.task == ic));
+%                         fprintf('- task is %s: \n', name{1});
+%                         fprintf('number of reps in all runs = %d\n', n_rep);
+%                         fprintf('numberof reps recorded in tsv = %d\n', n_rep_t);
+                        con                          = con/abs(sum(con));            
+                end % switch base
+
+                % set the name of the contrast
+                contrast_name = sprintf('%s-%s', char(unique(T.task_name(T.task == ic))), baseline);
+                SPM.xCon(idx) = spm_FcUtil('Set', contrast_name, 'T', 'c', con', SPM.xX.xKXs);
+                
+                idx = idx + 1;
+            end % ic (conditions)
+            
+            SPM = spm_contrasts(SPM,1:length(SPM.xCon));
+            save('SPM.mat', 'SPM','-v7.3');
+            SPM = rmfield(SPM,'xVi'); % 'xVi' take up a lot of space and slows down code!
+            save(fullfile(glm_dir, 'SPM_light.mat'), 'SPM')
+
+            % rename contrast images and spmT images
+            conName = {'con','spmT'};
+            for i = 1:length(SPM.xCon)
+                for n = 1:numel(conName)
+                    oldName = fullfile(glm_dir, sprintf('%s_%2.4d.nii',conName{n},i));
+                    newName = fullfile(glm_dir, sprintf('%s_%s.nii',conName{n},SPM.xCon(i).name));
+                    movefile(oldName, newName);
+                end % conditions (n, conName: con and spmT)
+            end % i (contrasts)
+        end % sn
+    
+>>>>>>> Stashed changes
 end
 
 

b118193b384183bfe451852df5f7f989811aaea6
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-03-25 19:30:28 -0400

    Update spmj_et1_et2_tert.m

--- spmj_et1_et2_tert.m
+++ spmj_et1_et2_tert.m
@@ -11,6 +11,9 @@ func1st = jsondecode(fileread(fullfile(funcDir, ...
     ['sub-' num2str(sn) '_task-task_run-01_bold.json'])));
 
 %% compute tert
+% total EPI readout time = = echo spacing (in ms) * base resolution 
+% (also knows as number of echos). If you use GRAPPA acceleration, 
+% you need to divide the total number of echos by two:
 base_resolution = func1st.BaseResolution;
 echo_spacing = func1st.EffectiveEchoSpacing * 1000;                        % compute echo spacing in milliseconds
 tert = echo_spacing * base_resolution / 2;

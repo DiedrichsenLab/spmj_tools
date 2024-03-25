48edc73dc065a2321964a3f05ff1a806b9417c7e
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-03-25 18:43:04 -0400

    Create spmj_et1_et2_tert.m
    This function retrieves the value of et1, et2 and tert from the metadata associated with imaging files

---
+++ spmj_et1_et2_tert.m
@@ -0,0 +1,29 @@
+function [et1, et2, tert] = spmj_et1_et2_tert(dataDir, subj_name, varargin)
+
+vararginoptions(varargin,{'sn'});
+
+fmapDir = fullfile(dataDir, "BIDS", subj_name, "fmap");
+funcDir = fullfile(dataDir, "BIDS", subj_name, "func");
+
+phasediff = jsondecode(fileread(fullfile(fmapDir, ...
+    ['sub-' num2str(sn) '_phasediff.json'])));
+func1st = jsondecode(fileread(fullfile(funcDir, ...
+    ['sub-' num2str(sn) '_task-task_run-01_bold.json'])));
+
+%% compute tert
+base_resolution = func1st.BaseResolution;
+echo_spacing = func1st.EffectiveEchoSpacing * 1000;                        % compute echo spacing in milliseconds
+tert = echo_spacing * base_resolution / 2;
+
+%% retrieve et1 et2 (in milliseconds)
+et1 = phasediff.EchoTime1 * 1000;
+et2 = phasediff.EchoTime2 * 1000;
+
+
+
+
+
+
+
+
+

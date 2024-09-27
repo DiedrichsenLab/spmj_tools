4f1292ee91b80794db5fe651d87776c174d1f8e9
incehusain <144962266+incehusain@users.noreply.github.com>
2024-09-27 09:29:15 -0700

    Update spmj_makesamealign_nifti.m

--- spmj_makesamealign_nifti.m
+++ spmj_makesamealign_nifti.m
@@ -1,14 +1,14 @@
 function spmj_makesamealign_nifti(P,Q)
 % function spmj_makesamealign_nifti(P,Q)
 % This function uses the affine transformation matrix from the source image 
-% and applies it to a 3d- or 4d- nifti file to give it the same affine transformation 
+% and applies it to a 4d- nifti file to give it the same affine transformation 
 % matrix. It does not change the voxel data of those images. 
 % It also deletes and <image>.mat files, where spm may have shuffled away
 % different matrices for different slice of a 4d- nifti. 
 % 
 % Input:
 %   P: name, vol-structure, or affine transformation matrix of the source image 
-%   Q: Cell array of imagenames of the 4d-nifti files to apply the affine
+%   Q: Char-array of imagenames of the 4d-nifti files to apply the affine
 %   matrix to. 
 if (nargin<1 || isempty(P)) 
     P=spm_select(1,'image','Select Image for orientation info'); 

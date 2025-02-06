a730bd49de79e2edec96d7591b324cbb4383bf6b
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2025-02-06 12:56:38 -0500

    Clarifications in fieldmap parameters

--- spmj_et1_et2_tert.m
+++ spmj_et1_et2_tert.m
@@ -6,7 +6,15 @@ function [et1, et2, tert] = spmj_et1_et2_tert(fmapDir, funcDir, subj_name)
 % MRI run. It then computes and returns the first and second echo times
 % (et1 and et2) in milliseconds, as well as the total effective EPI readout
 % time (tert) considering GRAPPA acceleration.
-%
+% 
+% You can verify these parameters in the scan protocol 
+% et1 = First echotime in ms of the field map (listed in contrast-common) 
+% et2 = Second echotime in ms of the field map (listed in contrast-common) 
+% total EPI readout time = = base resolution * echo spacing (in ms) / Accel. factor PE
+% Baseresolution: number of lines in k-space without undersampling 
+% echo spacing: time bewteen subsequent echos is ms: In Sequence - part 1
+% Accel Factor PE:  Resolution iPat 
+% 
 % Inputs:
 %   fmapDir: path to fmap in BIDS direcotry.
 %   funcDir: path to func in BIDS direcotry.
@@ -27,7 +35,7 @@ phasediff = jsondecode(fileread(fullfile(fmapDir, ...
 func1st = jsondecode(fileread(fullfile(funcDir, ...
     ['sub-' subj_name '_task-task_run-01_bold.json'])));
 
-%% compute tert
+% compute tert
 % total EPI readout time = = echo spacing (in ms) * base resolution 
 % (also knows as number of echos). If you use GRAPPA acceleration, 
 % you need to divide the total number of echos by two:
